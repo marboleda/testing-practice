2a87a90925591089edeb654a99eb3a3f202a8c1a
Marco Arboleda <arboleda.marco@gmail.com>
2020-10-21 22:02:21 -0400

    Write basic tests for calculator and implement it

--- functions.js
+++ functions.js
@@ -10,4 +10,12 @@ const reverseString = (input) => {
     return newString;
 }
 
-export { capitalizeString, reverseString }
+const calculator = {
+    add: (a,b) => { return a+b },
+    subtract: (a,b) => { return a-b },
+    divide: (a, b) => { return a/b },
+    multiply: (a, b) => { return a*b }
+
+}
+
+export { capitalizeString, reverseString, calculator }
--- functions.test.js
+++ functions.test.js
@@ -1,4 +1,4 @@
-import { capitalizeString, reverseString } from './functions';
+import { capitalizeString, reverseString, calculator } from './functions';
 
 //capitalizeString tests
 test('capitalize lowercase word', () => {
@@ -10,3 +10,21 @@ test('capitalize lowercase word', () => {
 test('reverse sentence', () => {
     expect(reverseString('Hi, my name is')).toBe('si eman ym ,iH');
 });
+
+
+//calculator tests
+test('basic addition', () => {
+    expect(calculator.add(2,5)).toBe(7);
+});
+
+test('basic subtraction', () => {
+    expect(calculator.subtract(5,2)).toBe(3);
+});
+
+test('basic division', () => {
+    expect(calculator.divide(10,2)).toBe(5);
+});
+
+test('basic multiplication', () => {
+    expect(calculator.multiply(3,6)).toBe(18);
+})
